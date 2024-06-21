648e387b869bbd6baef66bf5e63a551add4bc4a6
Mingfu Wang <43682165+morgan08yu@users.noreply.github.com>
2024-06-21 10:04:59 -0400

    Update Final_Aggregation.py

--- Final_Aggregation.py
+++ Final_Aggregation.py
@@ -6,6 +6,7 @@
 
 import numpy as np
 import pandas as pd
+from typing import Optional, Tuple
 
 
 # In[ ]:
@@ -23,39 +24,46 @@ def Agg_weight(weight_AHP):
 # In[ ]:
 
 
-def Reliability_Index (weight_vector, score_vector, QS, Ongoing_monitoring=False,data_drift=None, performance_drift=None):
-    '''
+def Reliability_Index(
+    weight_vector,
+    score_vector,
+    QS: float,
+    Ongoing_monitoring: bool = False,
+    data_drift: Optional[float] = None,
+    performance_drift: Optional[float] = None,
+)    -> Tuple[float, str]:
+    """
     Derive the final reliavility score and reliability index level by aggregating quantitative and qualitative components.
     ----------
-    Input arguments: 
+    Input arguments:
         'weight_vector': array, each value corresponds to a weight to one reliability component.
         'score_vector': array, quantitative score for each component.
         'QS': float (between 0 and 1), rate of yes answers of the qualitative questionnaire.
         'Ongoing_monitoring': boolean variable indicating whether information about ongoing monitoring phase is available.
         'data_drift': float, quantitative score associated with data drift in ongoing monitoring phase.
         'performance_drift': float, quantitative score associated with performance drift in ongoing monitoring phase.
-    ----------    
-    Return: 
+    ----------
+    Return:
         'RS': float (between 0 and 1), final reliavility score
-        'RI': ('High'/'Medium'/'Low'), final reliavility index level 
-    '''
-    
+        'RI': ('High'/'Medium'/'Low'), final reliavility index level
+    """
+
     RS_initial = np.sum(np.multiply(weight_vector, score_vector))
-    
+
     if Ongoing_monitoring == False:
-        RS = RS_initial*QS
+        RS = RS_initial * QS
     else:
-        M = (1- data_drift)*(1-performance_drift)
-        RS = RS_initial*QS*M
-        
-    if RS>= 0.8:
-        RI = 'High'
-    if 0.5 <= RS < 0.8:
-        RI = 'Medium'
-    elif RS <0.5:
-        RI = 'Low'
-    
-    return round(100*RS,2), RI
+        M = (1 - data_drift) * (1 - performance_drift)
+        RS = RS_initial * QS * M
+
+    if RS >= 0.8:
+        RI = "High"
+    elif 0.5 <= RS < 0.8:
+        RI = "Medium"
+    else:
+        RI = "Low"
+
+    return round(100 * RS, 2), RI
         
 
 
